d704426fa007604d9ed2b9d3c3db1ece87c0a016
eslamsheref5000 <eslamsheref5000@gmail.com>
2025-12-20 17:12:46 +0200

    Convert server config to JS to bypass TS build issues

---
+++ backend/config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1338),
+    app: {
+        keys: env.array('APP_KEYS'),
+    },
+    url: 'https://shando5000-dealz.hf.space',
+    proxy: true,
+});
+console.log('!!! JS SERVER CONFIG LOADED !!! URL:', 'https://shando5000-dealz.hf.space');
