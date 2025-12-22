f5fa0e7a4bf24d6b027699458550c84fd6a6d574
eslamsheref5000 <eslamsheref5000@gmail.com>
2025-12-22 13:05:56 +0200

    fix: improve react-simple-maps types for next dynamic

--- frontend/src/types/react-simple-maps.d.ts
+++ frontend/src/types/react-simple-maps.d.ts
@@ -1,1 +1,8 @@
-declare module 'react-simple-maps';
+declare module 'react-simple-maps' {
+    import * as React from 'react';
+    export const ComposableMap: React.ComponentType<any>;
+    export const Geometries: React.ComponentType<any>;
+    export const Geography: React.ComponentType<any>;
+    export const Marker: React.ComponentType<any>;
+    export const ZoomableGroup: React.ComponentType<any>;
+}
