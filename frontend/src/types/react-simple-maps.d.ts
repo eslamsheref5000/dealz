bcc7aa25ec3b57471efca442b8adcda6303e5bab
eslamsheref5000 <eslamsheref5000@gmail.com>
2025-12-23 04:58:10 +0200

    fix: update react-simple-maps type definition to use Geographies

--- frontend/src/types/react-simple-maps.d.ts
+++ frontend/src/types/react-simple-maps.d.ts
@@ -1,7 +1,7 @@
 declare module 'react-simple-maps' {
     import * as React from 'react';
     export const ComposableMap: React.ComponentType<any>;
-    export const Geometries: React.ComponentType<any>;
+    export const Geographies: React.ComponentType<any>;
     export const Geography: React.ComponentType<any>;
     export const Marker: React.ComponentType<any>;
     export const ZoomableGroup: React.ComponentType<any>;
