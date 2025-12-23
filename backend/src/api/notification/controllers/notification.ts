5204eeeee6cae780a1228e60ea5b71e671aa70a4
eslamsheref5000 <eslamsheref5000@gmail.com>
2025-12-23 05:09:04 +0200

    fix: enforce user filter in notification controller

--- backend/src/api/notification/controllers/notification.ts
+++ backend/src/api/notification/controllers/notification.ts
@@ -4,4 +4,25 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::notification.notification' as any);
+export default factories.createCoreController('api::notification.notification', ({ strapi }) => ({
+    async find(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You must be logged in to view notifications");
+        }
+
+        // Force filter by current authenticated user
+        ctx.query = {
+            ...ctx.query,
+            filters: {
+                ...(ctx.query.filters as object),
+                recipient: {
+                    id: user.id
+                }
+            }
+        };
+
+        return super.find(ctx);
+    }
+}));
